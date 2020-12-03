5eca1611eb7a7b64ddcb9182397b06c291febafb
Kyle Inch <kyle.inch@gm.com>
2020-12-03 16:10:10 -0500

    Day 3 Part 2 with some cleanup of semantic errors

--- day3/day3.py
+++ day3/day3.py
@@ -4,22 +4,23 @@ def traverseDownward(terrain_list, right_movement, down_movement):
 
     tree_hits = 0
 
-    while current_y < len(terrain_list):
+    while current_y < len(terrain_list) - 1:
         row = terrain_list[current_y]
 
-        landing_spot = terrain_list[current_y][current_x % (len(row) - 1)]
+        current_x = (current_x + right_movement) % (len(row) - 1)
+        current_y = current_y + down_movement
+        
+        landing_spot = terrain_list[current_y][current_x]
+
         if landing_spot == "#": 
             tree_hits = tree_hits + 1
+        elif landing_spot == ".":
+            a = 1
+        else: 
+            print("Unexpected!")
 
-        current_x = current_x + right_movement
-        current_y = current_y + down_movement
-    print("You hit", tree_hits, "trees!")
-
-
-
-
-
-
+    print("Got", tree_hits," tree hits on this slope")
+    return tree_hits
 
 
 with open('input.txt', 'r') as read_obj:
@@ -28,7 +29,12 @@ with open('input.txt', 'r') as read_obj:
     for row in read_obj:
         terrain_list.append(row)
 
-    traverseDownward(terrain_list, 3, 1)
+    slope1 = traverseDownward(terrain_list, 1, 1)
+    slope2 = traverseDownward(terrain_list, 3, 1)
+    slope3 = traverseDownward(terrain_list, 5, 1)
+    slope4 = traverseDownward(terrain_list, 7, 1)
+    slope5 = traverseDownward(terrain_list, 1, 2)
 
-
+    slope_product = slope1 * slope2 * slope3 * slope4 * slope5
+    print("The product of all the slope tree hits is:", slope_product)
 
