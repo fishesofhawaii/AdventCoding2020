edf38604b634840605513d0a1fb4e78e5168f809
Kyle Inch <kyle.inch@gm.com>
2020-12-08 17:12:27 -0500

    Added prompt, added part 2

--- day8/day8.py
+++ day8/day8.py
@@ -1,5 +1,38 @@
+def execute_program(operations, quantities, accumulator, row_list, index):
+    acc = accumulator
+    r_list = row_list.copy()
+    i = index
+    
+    while i not in r_list:
+        if i > len(operations) - 1:
+            break
+
+        r_list.append(i)
+
+        op = operations[i]
+        num = get_num(quantities[i])
+
+        if op == "acc":
+            acc += num
+            i += 1
+        elif op == "jmp":
+            i += num
+        elif op == "nop":
+            i += 1
+        else:
+            print("Dont know how to handle this", op)
+
+    if len(operations) == i:
+        print("Finished execution at", i, "accumulator:", acc)
+
 def get_num(raw_num):
     return int(raw_num)
+
+def get_modified_operations(operations, row, new_op):
+    copy_list = operations.copy()
+    copy_list[row] = new_op
+
+    return copy_list
 
 with open('input.txt', 'r') as read_obj:
     operations = []
@@ -11,12 +44,16 @@ with open('input.txt', 'r') as read_obj:
         operations.append(operation_quantity_list[0])
         quantities.append(operation_quantity_list[1])
 
-    print(operations)
-
-    row_list = []
+    
     index = 0
     accumulator = 0
+    row_list = []
+    print("THERE ARE A TOTAL OF", len(operations), "OPERATIONS")
+    # LETS BRUTE FORCE IT
     while index not in row_list:
+        if index > len(operations):
+            break
+
         row_list.append(index)
 
         op = operations[index]
@@ -26,12 +63,10 @@ with open('input.txt', 'r') as read_obj:
             accumulator += num
             index += 1
         elif op == "jmp":
+            execute_program(get_modified_operations(operations, index, "nop"), quantities, 0, [], 0)
             index += num
         elif op == "nop":
+            execute_program(get_modified_operations(operations, index, "jmp"), quantities, 0, [], 0)
             index += 1
         else:
             print("Dont know how to handle this", op)
-
-    print("The accumulator ended at", accumulator)
-
-    # ['nop', 'acc', 'jmp', 'acc', 'jmp', 'acc', 'acc', 'jmp', 'acc']
