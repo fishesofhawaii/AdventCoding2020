94e43db62fab1ff7e91208390dc36d529a11f46f
Kyle Inch <kyle.inch@gm.com>
2020-12-08 18:09:22 -0500

    Added logic, put a pin in it to come back to. Won't get distracted by football anymore

--- day7/day7.py
+++ day7/day7.py
@@ -1,38 +1,36 @@
+# INCOMPLETE
 class Bag:
     def __init__(self, color, size):
-        self.bags = []
+        self.parent_bags = []
         self.color = color
         self.size = size
    
-    def add_bag(self, bag):
-        self.bags.append(bag)
+    def add_parent(self, parent):
+        self.parent_bags.append(parent)
 
-    def print(self, indent):
-        print(" " * indent, self.color, self.size)
+    def print_bag(self, depth = 0):
+       print(" " * depth, "Color: {} Size: {}".format(self.color, self.size))
 
-    def print_bags(self, depth = 0):
-        for bag in self.bags:
-            bag.print(depth)
-            bag.print_bags(depth + 1)
+    def print_all(self, depth = 0):
+        self.print_bag(depth)
+        for parent in self.parent_bags:
+            parent.print_all(depth + 1)
 
-    def find(self, color):
-        for bag in self.bags:
-            if bag.color == color:
-                return bag
-            else:
-                return bag.find(color)
+    def find_bag(self, color):
+        if self.color == color:
+            return self
+        else: 
+            for parent_bag in self.parent_bags:
+                return parent_bag.find_bag(color)
 
         return None
-    
-def find_bag(bags, color):
-    for bag in bags.values():
-        found_bag = bag.find(color)
-        if found_bag != None:
-            return found_bag
-            
-    return None
 
-def get_bag_list(bags, raw_bag_string):
+    # def print_all(self, indent = 0):
+    #     self.print_bag(indent)
+    #     for parent in self.parent_bags:
+    #         parent.print_all(indent + 1)
+        
+def get_bag_list(raw_bag_string):
     bag_list = []
     if raw_bag_string == "no other bags":
         return bag_list
@@ -46,15 +44,16 @@ def get_bag_list(bags, raw_bag_string):
         assert bag_data_list[-1] in ["bag", "bags"] # Making sure input is as expected
         
         color_name = " ".join(bag_data_list[1:-1])
-        
-        bag = find_bag(bags, color_name)
-        if bag == None:
-            bag = Bag(color_name, int(bag_data_list[0]))
+        bag = Bag(color_name, int(bag_data_list[0]))
 
-        # Need to try to find the bag
         bag_list.append(bag)
 
     return bag_list
+
+def populate_bag_set(bag, bag_set):
+    bag_set.add(bag.color)
+    for parent in bag.parent_bags:
+        populate_bag_set(parent, bag_set)
 
 with open('input.txt', 'r') as read_obj:
     bags = {}
@@ -69,20 +68,22 @@ with open('input.txt', 'r') as read_obj:
         color = color_bag_count_split[0].strip()
         input_bag = Bag(color, 1)
         raw_bag_string = color_bag_count_split[1].replace(".", "").strip()
-        bag_list = get_bag_list(bags, raw_bag_string)
+        bag_list = get_bag_list(raw_bag_string)
 
         for bag in bag_list:
-            input_bag.add_bag(bag)
+            bag.add_parent(input_bag)
+            bags[bag.color] = bag
 
-        was_added = False
-        for bag in bags.values():
-            found_bag = bag.find(input_bag.color)
-            if found_bag != None:
-                found_bag.add_bag(input_bag)
-                was_added = True
-        if not was_added:
-            bags[color] = input_bag
+    # a = Bag("a", 1)
+    # b = Bag("b", 1)
+    # c = Bag("c", 1)
+    # d = Bag("d", 1)
 
+    # a.add_parent(b)
+    # b.add_parent(c)
+    # c.add_parent(d)
 
-    for bag in bags.values():
-        bag.print_bags()
+    # bags["a"] = a
+
+    # for bag in bags.values():
+    #     bag.print_bags()
