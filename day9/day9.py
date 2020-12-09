de222815d58e46fff2405b2d152a4beeb7693184
Kyle Inch <kyle.inch@gm.com>
2020-12-09 16:57:19 -0500

    Part 2 complete

--- day9/day9.py
+++ day9/day9.py
@@ -1,3 +1,19 @@
+def find_encryption_weakness(number, data_list): 
+    index = 0
+    while index < len(data_list):
+        rolling_list = []
+        current_index = index
+
+        while current_index < len(data_list):
+            rolling_list.append(data_list[current_index])
+            if sum(rolling_list) == number:
+                return min(rolling_list) + max(rolling_list)
+            elif sum(rolling_list) > number:
+                break # break the inside loop
+            current_index += 1
+
+        index += 1
+
 with open('input.txt', 'r') as read_obj:
     preamble_size = 25
     data_list = []
@@ -21,3 +37,6 @@ with open('input.txt', 'r') as read_obj:
             break
 
         index += 1
+
+    response = find_encryption_weakness(data_list[index], data_list)
+    print("Final answer", response)
