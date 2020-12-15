d8560e7094d51cb8e316068e4287050196fe625e
Kyle Inch <kyleinch@Kyles-MacBook-Pro.local>
2020-12-14 20:47:43 -0500

    Part 2 complete

--- day14/day14.py
+++ day14/day14.py
@@ -1,19 +1,38 @@
 def get_response(mask, binary_string):
     index = 0
     response_string = ""
+
     while index < len(mask):
         if mask[index] == binary_string[index]:
             response_string += mask[index]
-        elif mask[index] == "0":
-            response_string += "0"
         elif mask[index] == "1":
             response_string += "1"
+        elif mask[index] == "X":
+            response_string += "X"
         else: 
             response_string += binary_string[index]
         
         index += 1
 
-    return response_string
+
+    response_list = [response_string]
+    x_index = response_string.find("X")
+
+    while x_index != -1:
+        temp_list = []
+        for response in response_list:
+            l_0 = list(response)
+            l_1 = list(response)
+
+            l_0[x_index] = "0"
+            l_1[x_index] = "1"
+
+            temp_list += ["".join(l_0), "".join(l_1)]
+        
+        response_list = temp_list.copy()
+        x_index = response_list[0].find("X")
+
+    return response_list
         
 def get_mem_slot(s):
     # Comes in as "mem[####]" we just want ####
@@ -39,12 +58,15 @@ with open('input.txt', 'r') as read_obj:
             mask = get_mask_value(right_side)
         else:
             slot = get_mem_slot(left_side)
-            value = get_binary_value(right_side)
-            res = get_response(mask, value)
-            member_berries[slot] = eval(res)
+            binary_slot = get_binary_value(get_mem_slot(left_side))
+            # # Need to decode for part 2
+            slot_list = get_response(mask, binary_slot)
+
+            for slot in slot_list:
+                member_berries[eval(slot)] = int(right_side)
 
     total = 0
-    for v in member_berries.values():
+    for k, v in member_berries.items():
         total += v
 
     print(total)
